997850ed9cef617f12f71ec564aabeb3f0e625d9
Ashok <ashok@speelyaal.com>
2020-03-03 17:54:25 +0100

    Initial Spring boot Project

---
+++ thaalam-spring-boot/src/main/kotlin/com/speelyaal/thaalam/ThaalamApplication.kt
@@ -0,0 +1,11 @@
+package com.speelyaal.thaalam
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ThaalamApplication
+
+fun main(args: Array<String>) {
+	runApplication<ThaalamApplication>(*args)
+}
---
+++ thaalam-spring-boot/src/test/kotlin/com/speelyaal/thaalam/ThaalamApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.speelyaal.thaalam
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ThaalamApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
